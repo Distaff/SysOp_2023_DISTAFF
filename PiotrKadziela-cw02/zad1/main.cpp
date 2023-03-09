321b0de84aaa9a1c00ee8042e5d202a9622dcca9
Distaff <distaff.pk@gmail.com>
2023-03-09 18:35:18 +0100

    cw2/zad1 dne, cw2/zad2 WIP

--- PiotrKadziela-cw02/zad1/main.cpp
+++ PiotrKadziela-cw02/zad1/main.cpp
@@ -1,101 +1,117 @@
-﻿#include "fileData.h"
-#include <iostream>
-#include <ios>
+﻿#include <iostream>
+#include <iomanip>
 #include <string>
-#include <sstream>
-#include <time.h>
+#include <fstream>
+#include <vector>
+#include <chrono>
+
+#include <fcntl.h> 
+#include <unistd.h> 
+#include <sys/types.h> 
+#include <sys/stat.h>
+
+constexpr int BUFFER_SIZE = 4096;
 
 using namespace std::string_literals;	//for "foobar"s style std::string literals
 
-int getIntOrThrow(std::istringstream& iss) {
-	int res = 0xDEADBEFF;
-	iss >> res;
-	if (iss.fail()) {
-		std::string buff = iss.str();
-		throw std::runtime_error("Integer value expected in: "s + buff);
-	}
+std::string md5OfFile(std::string path) {
+	int errCode = system(("md5sum "s + path + " > /tmp/md5OfFile").c_str());
+	if (errCode)
+		throw std::system_error(errCode, std::system_category(), "Error encountered when using md5sum"s);
+
+	std::ifstream md5File("/tmp/md5OfFile");
+	if (!md5File) 
+		throw std::runtime_error("Error: cannot open md5 temp file."s);
+	std::string res;
+	md5File >> res;
 	return res;
 }
 
-int main()
-{
-	fileData* fd = nullptr;
-	std::string buff;
-	std::string linestr;
-	std::istringstream line;
-	int param = 0xDEADBEFF;
+void copyFileWithReplacement(char toReplace, char replacement, std::ifstream &inFile, std::ofstream &outFile) {
+	std::vector<char> buff(BUFFER_SIZE);
+	int bytesRead = 0xDEADBEEF;
 
+	while (inFile) {
+		bytesRead = inFile.read(&buff[0], buff.size()).gcount();
 
-	timespec realBeginTime;
-	timespec userBeginTime;
-	timespec systemBeginTime;
-	timespec realEndTime;
-	timespec userEndTime;
-	timespec systemEndTime;
+		for (auto& i : buff)
+			if (i == toReplace) i = replacement;
 
-	while (std::cin.peek() != EOF) {
-		clock_gettime(CLOCK_REALTIME, &realBeginTime);
-		clock_gettime(CLOCK_REALTIME, &userBeginTime);
-		clock_gettime(CLOCK_REALTIME, &systemBeginTime);
-
-
-		std::getline(std::cin, linestr);
-		line.str(linestr);
-		line.clear();
-		line.seekg(0, std::ios::beg);
-
-		line >> buff;
-
-		if (buff == "init") {
-			param = getIntOrThrow(line);
-			fd = new fileData(param);
-			if (line >> buff)
-				throw std::runtime_error("Unexpected parameter: "s + buff);
-		}
-		else if (buff == "count") {
-			line >> buff;
-
-			fd->pushFromFile(buff);
-
-			if (line >> buff)
-				throw std::runtime_error("Unexpected parameter: "s + buff);
-		}
-		else if (buff == "show") {
-			param = getIntOrThrow(line);
-
-			singleFileData &ref = (*fd)[param];
-			std::cout << "\nRecord "s << param << ":\n"
-				<< "  lines: "s << ref.lines << '\n'
-				<< "  words: "s << ref.words << '\n'
-				<< "  chars: "s << ref.chars << '\n';
-
-			if (line >> buff)
-				throw std::runtime_error("Unexpected parameter: "s + buff);
-		}
-		else if (buff == "delete") {
-			while (line.peek() != EOF && line.peek() != '\n' && line.peek() != '\r') {
-				param = getIntOrThrow(line);
-				fd->erase(param);
-			}
-
-		}
-		else if (buff == "destroy") {
-			delete fd;
-			if (line >> buff)
-				throw std::runtime_error("Unexpected parameter: "s + buff);
-		}
-		else {
-			throw std::runtime_error("Unrecognized command: "s + buff);
-		}
-
-		clock_gettime(CLOCK_REALTIME, &realEndTime);
-		clock_gettime(CLOCK_REALTIME, &userEndTime);
-		clock_gettime(CLOCK_REALTIME, &systemEndTime);
-
-		std::cout << "Operation '" << linestr.erase(linestr.length() - 1) << "' finished in: \n"
-			<< (realEndTime.tv_nsec - realBeginTime.tv_nsec) * 0.001 << "us realtime\n  "
-			<< (userEndTime.tv_nsec - userBeginTime.tv_nsec) * 0.001 << "us usertime\n  "
-			<< (systemEndTime.tv_nsec - systemBeginTime.tv_nsec) * 0.001 << "us systemtime\n\n";
+		outFile.write(&buff[0], bytesRead);
 	}
+}
+
+void copyFileWithReplacement(char toReplace, char replacement, int inFile, int outFile) {
+	std::vector<char> buff(BUFFER_SIZE);
+	int bytesRead = 0xDEADBEEF;
+
+	while (bytesRead) {
+		bytesRead = read(inFile, buff.data(), buff.size());
+
+		for (auto& i : buff)
+			if (i == toReplace) i = replacement;
+
+		write(inFile, buff.data(), bytesRead);
+	}
+}
+
+int main(int argc, char** argv)
+{
+	std::vector<std::string> args(argv + 1, argv + argc);
+	auto start = std::chrono::high_resolution_clock::now();
+	auto stop = std::chrono::high_resolution_clock::now();
+	int durationMicros = 0xDEADBEEF;
+	std::string md5;
+
+	if (args.size() != 4 || args[0].length() != 1 || args[1].length() != 1)
+		throw std::runtime_error("Imporper arguments! Expected: char, char, string, string"s);
+
+
+	std::ifstream std_inFile(args[2]);
+	std::ofstream std_outFile(args[3]);
+	int posix_inFile = open(args[2].c_str(), O_RDONLY);
+	int posix_outFile = open(args[3].c_str(), O_WRONLY | O_TRUNC);
+
+	if (!std_inFile)
+		throw std::runtime_error("Error: cannot open fstream of input file."s);
+	if (!std_outFile)
+		throw std::runtime_error("Error: cannot open fstream of output file."s);
+	if (!posix_inFile)
+		throw std::runtime_error("Error: cannot open POSIX input file."s);
+	if (!posix_outFile)
+		throw std::runtime_error("Error: cannot open POSIX output file."s);
+
+
+	/* STD LIBRARY TESTING */
+
+	std::cout << "\nReplacing chars using std...\n";
+
+	start = std::chrono::high_resolution_clock::now();
+	copyFileWithReplacement(args[0][0], args[1][0], std_inFile, std_outFile);
+	stop = std::chrono::high_resolution_clock::now();
+
+	durationMicros = std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count();
+	std::cout << "Replacing done. \n"
+		<< " Elapsed time: " << std::fixed << std::setprecision(3) << durationMicros / 1000.0 << "ms\n";
+
+	md5 = md5OfFile(args[3]);
+
+
+	/* POSIX SYSTEM CALLS TESTING */
+
+	std::cout << "\nReplacing chars using POSIX system calls...\n";
+
+	start = std::chrono::high_resolution_clock::now();
+	copyFileWithReplacement(args[0][0], args[1][0], posix_inFile, posix_outFile);
+	stop = std::chrono::high_resolution_clock::now();
+
+	durationMicros = std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count();
+	std::cout << "Replacing done. \n"
+		<< " Elapsed time: " << std::fixed << std::setprecision(3) << durationMicros / 1000.0 << "ms\n";
+
+
+
+	std::cout << ((md5 == md5OfFile(args[3])) ? "\nFiles are identical." : "\nERROR: files are not identical.");
+
 	return 0;
 }
